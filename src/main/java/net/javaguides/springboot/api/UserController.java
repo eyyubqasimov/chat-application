6b2c09dbffc1b86e9a9bd2d46767b41a797366ad
eyyubqasimov <eyyub.qasimov98@gmail.com>
2023-08-18 10:21:39 +0400

    last time

--- src/main/java/net/javaguides/springboot/api/UserController.java
+++ src/main/java/net/javaguides/springboot/api/UserController.java
@@ -1,9 +1,5 @@
 package net.javaguides.springboot.api;
-
-import java.awt.PageAttributes.MediaType;
-import java.util.List;
 import java.util.Set;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
 import net.javaguides.springboot.entity.ActivityLogEntity;
 import net.javaguides.springboot.entity.User;
 import net.javaguides.springboot.enums.ActivityType;
